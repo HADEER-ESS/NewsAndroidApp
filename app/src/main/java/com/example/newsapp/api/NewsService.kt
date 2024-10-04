1dff63272d3187ae786c92d1340e2a8d18994e6a
hadeer <hadeeressam21@gmail.com>
2024-10-05 00:00:20 +0300

    + create Service interface (where api requests place), and Manager object to define retorfit and how want to response the data using OkHttpClient

---
+++ app/src/main/java/com/example/newsapp/api/NewsService.kt
@@ -0,0 +1,24 @@
+package com.example.newsapp.api
+
+import com.example.newsapp.api.model.CategorySourceResponse
+import com.example.newsapp.api.model.NewsSourceByCategoryResponse
+import com.example.newsapp.api.model.NewsSourceResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+//Where will place all urls for api (Methods)
+interface NewsService {
+
+    @GET("top-headlines/sources")
+    fun getSourcesBasedOnCategory(
+        @Query("category") category: String,
+        @Query("apiKey") api_key : String = "cfbdb104ea384558b5df3642c9b44917"
+    ): Call<NewsSourceByCategoryResponse>
+
+    @GET("top-headlines")
+    fun getNewsBySource(
+        @Query("sources") source : String,
+        @Query("apiKey") api_key : String = "cfbdb104ea384558b5df3642c9b44917"
+    ):Call<NewsSourceResponse>
+}
