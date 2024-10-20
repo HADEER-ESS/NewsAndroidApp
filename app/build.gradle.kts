0ef9fd258201f0ab13486bdbb8b6d0fce553f538
hadeer <hadeeressam21@gmail.com>
2024-10-20 22:28:11 +0300

    $ try to applu changes language in Android12 and below!!

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,7 @@ android {
 }
 
 dependencies {
-    val nav_version = "2.8.1"
+    implementation(libs.androidx.appcompat)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
